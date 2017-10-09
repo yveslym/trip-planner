7b8a119b9bc0bd45b798bba0e750533eb666ff85
yves songolo <yves.songolo@gmail.com>
2017-10-08 22:52:39 -0700

    trip class

--- server.py
+++ server.py
@@ -7,7 +7,6 @@ import bcrypt
 from mongoengine import *
 import pdb
 
-
 app = Flask(__name__)
 mongo = MongoClient('localhost', 27017)
 app.db = mongo.trip_planner_test
@@ -17,15 +16,77 @@ api = Api(app)
 
 ## Write Resources here
 
+
+# class Pre_Trip1(Document):
+#     pass
+# class Pre_Trip2(Resource):
+#     pass
+# class A:
+#     __metaclass__ = Pre_Trip1
+# class B:
+#     __metaclass__ = Pre_Trip2
+#
+# class Pre_Trip3(Pre_Trip1, Pre_Trip2):
+#     pass
+#
+# class Trip(A,B):
+#     __metaclass__ = Pre_Trip3
+#
+#
+#     def test(self):
+#         print('trip class work')
+
+class Trip(Resource):
+    def __init__(self,name,destination):
+        self.name = name
+        self.destination = destination
+        self.stop_point = ListField()
+        self.completed = False
+        self.start_date = StringField(max_length=20)
+        self.user = ReferenceField(User)
+
+    def post(self):
+        trip_collect = app.db.posts
+        trip_json = request.json
+        name = request.json('name')
+        destination = request.json('destination')
+        user = request.jason('user')
+
+        if name is None or destination is None or user is None:
+            return({'error':'both name and destion are required to create a post'},400,None)
+        else:
+            self.name = name
+            self.destination = destination
+            self.user = user
+            self.completed = False
+
+        if trip_json['stop_point'] is not None:
+            self.stop_point = trip_json('stop_point')
+
+        if trip_json['start_date'] is not None:
+            self.start_date = trip_json['start_date']
+
+        trip_dict = {self.name,
+                     self.destination,
+                     self.user,
+                     self.stop_point,
+                     self.completed,
+                     self.start_date}
+        trip_collect.insert_one(trip_dict)
+        return (trip_dict,200,None)
+
+
+
+
+
+
 class User(Resource):
-
-    first_name = ""#StringField(required=True, max_length=30)
-    last_name = ""#StringField(required=True, max_length=30)
-    email = ""#EmailField(required=True, unique=True)
-    password = ""#StringField (required=True, max_length=30)
-    username = ""#StringField(max_length=30)
+    first_name = ""  # StringField(required=True, max_length=30)
+    last_name = ""  # StringField(required=True, max_length=30)
+    email = ""  # EmailField(required=True, unique=True)
+    password = ""  # StringField (required=True, max_length=30)
+    username = ""  # StringField(max_length=30)
     country = ""
-
 
     def post(self):
         user_dict = request.json
@@ -35,51 +96,75 @@ class User(Resource):
             post = user_collect.insert_one(user_dict)
             return (post, 201, None)
         elif 'email' in user_dict is None:
-            return ({'error':'no email provided'}, 400, None)
+            return ({'error': 'no email provided'}, 400, None)
         elif 'password' in user_dict is None:
-            return ({'error':'Not password provided'}, 400, None)
+            return ({'error': 'Not password provided'}, 400, None)
         elif 'first_name' in user_dict is None or 'last_name' in user_dict is None:
-            return ({'error':'either first name or last name were not passed'}, 400, None)
+            return ({'error': 'either first name or last name were not passed'}, 400, None)
         else:
-            return ({'error':'there is other error'},400, None)
+            return ({'error': 'there is other error'}, 400, None)
 
     def get(self):
 
-        user_country = request.args.get('country')
-        print(user_country)
-        if user_country is None:
-            print ('country ',user_country)
-            return ({'error':'country was not  found in the database'}, 404,None)
+        user_email = request.args.get('email')
+        print(user_email)
+        if user_email is None:
+            print('country ', user_email)
+            return ({'error': 'user was not found in the database'}, 404, None)
         user_collect = app.db.users
         # pdb.set_trace()
-        user_dict = user_collect.find({'country': user_country})
+        user_dict = user_collect.find_one({'email': user_email})
         print(user_dict)
         if user_dict is None:
-            return ({'error':'user does not exist'}, 404, None)
+            return ({'error': 'user does not exist'}, 404, None)
         else:
-            arr = []
-            for user in user_dict:
-                arr.append(user)
+            # arr = []
+            # for user in user_dict:
+            #     arr.append(user)
 
-            return (arr,200, None)
-
-
+            return (user_dict, 200, None)
 
     def patch(self):
+        user_email = request.arg.get('email')
+        user_json = request.json
+        if user_email is None:
+            return ({'error': 'user not found'}, 404, None)
 
+        user_collect = app.db.users
+        user_dict = user_collect.find_one({'email': user_email})
 
-     def is_user_exist(self, email):
+        if 'first_name' in user_json is not None:
+            user_dict['first_name'] = user_json['first_name']
+        elif user_json['email'] is not None:
+            user_dict['email'] = user_json['email']
+        elif user_json['last_name'] is not None:
+            user_dict['last_name'] = user_json['last_name']
+        elif user_json['user_name'] is not None:
+            user_dict['username'] = user_json['username']
+        elif user_json['password'] is not None:
+            user_dict['password'] = user_json['password']
+        else:
+            return ({'error': 'no argument was passed to be save'}, 404, None)
+
+        user_collect.save(user_dict)
+        return (user_dict, 200, None)
+
+    def put(self):
+
+        user_email = request.arg
+
+    def is_user_exist(self, email):
         user_collect = app.db.users
         user_dict = user_collect.find_one({'email': email})
 
         if user_dict is None:
 
-            user_dict = {'first_name':self.first_name,
-                         'last_name':self.last_name,
+            user_dict = {'first_name': self.first_name,
+                         'last_name': self.last_name,
                          'email': self.email,
-                         'password':self.password,
-                         'username':self.username,
-                         'country':self.country}
+                         'password': self.password,
+                         'username': self.username,
+                         'country': self.country}
             user_collect.insert_one(user_dict)
 
             return False
@@ -87,12 +172,10 @@ class User(Resource):
             return True
 
 
-
-
-
 ## Add api routes here
 
 api.add_resource(User, '/users')
+
 
 #  Custom JSON serializer for flask_restful
 @api.representation('application/json')
@@ -101,7 +184,10 @@ def output_json(data, code, headers=None):
     resp.headers.extend(headers or {})
     return resp
 
+
 if __name__ == '__main__':
+    trips = Trip()
+    trips.test()
     # Turn this on in debug mode to get detailled information about request
     # related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
